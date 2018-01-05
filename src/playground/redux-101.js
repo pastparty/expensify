b84c77151419a79b91663c8255ba216160ba4eb7
csippel <christoph.sippel@glossybox.com>
2018-01-05 18:04:26 +0100

    getting started with redux

---
+++ src/playground/redux-101.js
@@ -0,0 +1,52 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT': {
+            return {
+                count: state.count + 1
+            };
+        }
+        case 'DECREMENT': {
+            return {
+                count: state.count - 1
+            };
+        }
+        case 'RESET': {
+            return {
+                count: 0
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+});
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+// unsubscribe();
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
